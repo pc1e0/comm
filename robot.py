0ca40ea7820439959f36ae1c99d8803486a857f8
pc1e0 <pc1e0dev@gmail.com>
2023-09-21 01:03:55 +0300

    Made initial commit

---
+++ robot.py
@@ -0,0 +1,57 @@
+import os
+import praw
+import threading
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class EthTraderAI:
+
+
+    def __init__(self, client_id, client_secret, username, password, user_agent, subreddit):
+
+        self.reddit = praw.Reddit(
+            client_id=client_id,
+            client_secret=client_secret,
+            username=username,
+            password=password,
+            user_agent=user_agent
+        )
+        self.subreddit = self.reddit.subreddit(subreddit)
+
+    
+    def listen_to_comments(self):
+        for comment in self.subreddit.stream.comments(skip_existing=True):
+            print(f"New comment: [{comment.author.name}] {comment.body}")
+
+
+    def listen_to_posts(self):
+        for submission in self.subreddit.stream.submissions(skip_existing=True):
+            print(f"New post: [{submission.author.name}] {submission.title}: {submission.selftext if submission.is_self else submission.url}")
+
+
+    def listen_to_subreddit(self):
+        comment_thread = threading.Thread(target=self.listen_to_comments)
+        post_thread = threading.Thread(target=self.listen_to_posts)
+
+        comment_thread.start()
+        post_thread.start()
+
+
+if __name__ == '__main__':
+
+    # Load Reddit keys
+    CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
+    CLIENT_SECRET = os.getenv("REDDIT_SECRET")
+    USERNAME = os.getenv('REDDIT_USERNAME')
+    PASSWORD = os.getenv('REDDIT_PASSWORD')
+    USER_AGENT = os.getenv('REDDIT_USER_AGENT')
+    SUBREDDIT = os.getenv('REDDIT_SUBREDDIT')
+
+    # Initialize robot
+    robot = EthTraderAI(CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD, USER_AGENT, SUBREDDIT)
+
+    # Start listening to the EthTrader subreddit
+    robot.listen_to_subreddit()
