22f0f9b590a869df8e24d2ca33240c868a5680ba
pc1e0 <pc1e0dev@gmail.com>
2023-09-24 02:36:10 +0300

    Refactored Reddit robot

--- robot.py
+++ robot.py
@@ -1,7 +1,8 @@
 import os
 import praw
-import openai
 import threading
+from ai import ChatGPT
+from db import Weaviate
 from urllib.parse import urlparse
 from dotenv import load_dotenv
 
@@ -13,7 +14,6 @@ class EthTraderAI:
 
     def __init__(self):
 
-        openai.api_key = os.getenv("OPENAI_KEY")
         client_id = os.getenv("REDDIT_CLIENT_ID")
         client_secret = os.getenv("REDDIT_SECRET")
         username = os.getenv('REDDIT_USERNAME')
@@ -30,42 +30,52 @@ class EthTraderAI:
         )
         self.subreddit = self.reddit.subreddit(subreddit)
 
-
-    def check_content_moderation(self, content):
-        response = openai.Moderation.create(input=content)
-        return response["results"][0]['flagged']
+        self.ai = ChatGPT()
+        self.db = Weaviate()
 
 
     def extract_context(self, comment, depth=5):
+
         context = []
         current_comment = comment
+
         for _ in range(depth):
+
             if isinstance(current_comment, praw.models.Comment):
                 context.insert(0, f"{current_comment.id}: comment | {current_comment.author.name}: {current_comment.body}")
+
             elif isinstance(current_comment, praw.models.Submission):
                 context.insert(0, f"{current_comment.id}: post | {current_comment.author.name}: {current_comment.title}")
+
             try:
                 current_comment = current_comment.parent()
+
             except AttributeError:
                 break  # We reached the top
+
         return context
 
     
     def listen_to_comments(self):
+
         for comment in self.subreddit.stream.comments(skip_existing=True):
+
             parent_comments = self.extract_context(comment)
             full_context = '\n'.join(parent_comments)
-            flagged = self.check_content_moderation(full_context)
+            flagged = self.ai.moderate(full_context)
+
             if not flagged:
                 print(full_context)
 
 
     def listen_to_posts(self):
+
         for submission in self.subreddit.stream.submissions(skip_existing=True):
             print(f"New post: [{submission.author.name}] {submission.title}: {submission.selftext if submission.is_self else submission.url}")
 
 
     def listen_to_subreddit(self):
+        
         comment_thread = threading.Thread(target=self.listen_to_comments)
         post_thread = threading.Thread(target=self.listen_to_posts)
 
